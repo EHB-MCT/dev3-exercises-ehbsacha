8cdd368cf756c3f17644fe52c171ac060200f34c
ehbsacha <sacha.voskuilen@student.ehb.be>
2021-10-12 13:59:58 +0200

    delete idea and part 2 of the game

--- session2/S2 minigame.kt
+++ session2/S2 minigame.kt
@@ -1,4 +1,7 @@
+var i = 0;
+
 fun main(){
+
     roomOne()
 }
 
@@ -7,13 +10,14 @@ fun roomOne(){
     println("A cold stone wall against your back.")
     println("At the wall on your left there's a yellow door, in front of you a red one and to your right a blue one.")
     print("Which door do you choose? ")
-    var i = 0;
+
     var inputOne = readLine()
     do {
         when (inputOne) {
             "yellow" -> {
                 i++
                 alive()
+                roomTwo()
             }
             "red" -> {
                 i++
@@ -32,6 +36,23 @@ fun roomOne(){
     } while (i != 1)
 }
 
+fun roomTwo(){
+    println("The second room is better lit.")
+    println("A Black cat on the other side stares you in the eyes!")
+    println("There are even more doors but with numbers written on them. 1 to 5")
+    print("Which number door do you choose? (As a number like '1' or '2'): ")
+
+    var inputTwo = readLine()!!.toInt()
+    val randomNumber = (1..5).random()
+    if(inputTwo == randomNumber){
+        i++
+        alive()
+    } else if(inputTwo == 1 || inputTwo == 2 || inputTwo == 3 || inputTwo == 4 || inputTwo == 5 && inputTwo != randomNumber){
+        i++
+        dead()
+    }
+}
+
 fun alive(){
     println("You survived")
     println("FOR NOW!")
@@ -39,5 +60,8 @@ fun alive(){
 
 fun dead(){
     println("You died a horrible death.")
-    print("The door fell on you")
+    when (i){
+        1 -> println("The door fell on you")
+        2 -> println("The door was locked and the cat ate you!")
+    }
 }
