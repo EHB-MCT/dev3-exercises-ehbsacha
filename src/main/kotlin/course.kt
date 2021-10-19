869e59ba96496ece42f226d7f35dcb59387fb807
ehbsacha <sacha.voskuilen@student.ehb.be>
2021-10-19 09:47:12 +0200

    Classes for courses

---
+++ src/main/kotlin/course.kt
@@ -0,0 +1,12 @@
+class Course(
+    val courseName: String,
+    val teacher: String,
+    val description: String,
+    val credits: Int,
+    val semester: Int,
+    val studyYear: Int,
+    val module: String,
+    val required: Boolean,
+    val requirements: String
+    ){
+}
---
+++ src/main/kotlin/ehb.kt
@@ -0,0 +1,15 @@
+// course.kt
+
+fun main(){
+    val design1 = Course(
+        "Design",
+        "Maaike Beuten",
+        "A fine course",
+        5,
+        1,
+        1,
+        "Design",
+        true,
+        "none"
+    )
+}
