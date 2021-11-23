201bd90bf86b50096bc0cfd602caf6a2a0650d54
ehbsacha <sacha.voskuilen@student.ehb.be>
2021-11-23 11:18:08 +0100

    db werkend
    database werkt, commit voor update credentials

--- session6/public_transport/src/main/kotlin/Main.kt
+++ session6/public_transport/src/main/kotlin/Main.kt
@@ -1,3 +1,23 @@
+import java.sql.*
+import java.util.*
+
 fun main(){
+    Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance()
+
+    val credentials = Credentials()
+
+    // Prepare credentials
+    val connectionProps = Properties()
+    connectionProps["user"] = credentials.userName
+    connectionProps["password"] = credentials.password
+
+    // Create the connection: this will allow us to run queries on it later
+    val connection =  DriverManager.getConnection(
+        "jdbc:" + "mysql" + "://" +
+                credentials.host +
+                ":" + "3306" + "/" +
+                credentials.databaseName,
+        connectionProps)
+
     println("Hello World!")
 }
