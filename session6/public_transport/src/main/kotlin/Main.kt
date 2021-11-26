8ff2633fbf1706415da569b102ed5593c431e90f
ehbsacha <sacha.voskuilen@student.ehb.be>
2021-11-26 12:46:31 +0100

    Select city
    You can select a city

--- session6/public_transport/src/main/kotlin/Main.kt
+++ session6/public_transport/src/main/kotlin/Main.kt
@@ -1,16 +1,19 @@
 import java.sql.*
 import java.util.*
 
+val credentials = Credentials()
+
 fun main(){
+    //Get a user input
+    print("Type the city you want to go to ")
+    val userinput = readLine()
+
+    //Make connection with database
     Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance()
-
-    val credentials = Credentials()
-
     // Prepare credentials
     val connectionProps = Properties()
     connectionProps["user"] = credentials.userName
     connectionProps["password"] = credentials.password
-
     // Create the connection: this will allow us to run queries on it later
     val connection =  DriverManager.getConnection(
         "jdbc:" + "mysql" + "://" +
@@ -19,5 +22,15 @@ fun main(){
                 credentials.databaseName,
         connectionProps)
 
-    println("Hello World!")
+    //Select statement and filtering
+    val statement = connection.prepareStatement("SELECT * FROM rides, cities where rides.destination_city_id = cities.id and cities.name = ? ORDER BY rides.departure_time DESC LIMIT 1")
+    statement.setString(1, userinput)
+//    and LIKE NOW()
+    val result = statement.executeQuery()
+
+    while(result.next()) {
+        println(result.getString("departure_time"))
+    }
+
+
 }
