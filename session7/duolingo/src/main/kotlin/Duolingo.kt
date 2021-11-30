943e2463bcc1bb17f3e233ae73dc8b2057d19f2d
ehbsacha <sacha.voskuilen@student.ehb.be>
2021-11-30 11:09:32 +0100

    Randomized and only 5
    randomized the wordlist and selected only 5 to learn

--- session7/duolingo/src/main/kotlin/Duolingo.kt
+++ session7/duolingo/src/main/kotlin/Duolingo.kt
@@ -1,33 +1,35 @@
 class Duolingo(
 ){
-    init{
-        val wordlist = arrayOf(
-            Word("Niño","jongen","spaans"),
-            Word("Niña","meisje","spaans"),
-            Word("boleto","ticket","spaans"),
-            Word("manzanas","appels","spaans"),
-            Word("maleta","koffer","spaans"),
-            Word("aeropuerto","luchthaven","spaans"),
-            Word("pan","brood","spaans"),
-            Word("cerrado","gesloten","spaans"),
-            Word("gato","kat","spaans"),
-            Word("perro","hond","spaans"),
+    val words = mutableListOf<Word>(
+        Word("Niño","jongen","spaans"),
+        Word("Niña","meisje","spaans"),
+        Word("boleto","ticket","spaans"),
+        Word("manzanas","appels","spaans"),
+        Word("maleta","koffer","spaans"),
+        Word("aeropuerto","luchthaven","spaans"),
+        Word("pan","brood","spaans"),
+        Word("cerrado","gesloten","spaans"),
+        Word("gato","kat","spaans"),
+        Word("perro","hond","spaans"),
 
-            Word("bil","auto","zweeds"),
-            Word("Vän","vriend","zweeds"),
-            Word("Bra","goed","zweeds"),
-            Word("Tystnad","stilte","zweeds"),
-            Word("Duktig","getalenteerd","zweeds"),
-            Word("Älskar","liefde","zweeds"),
-            Word("Leende","glimlach","zweeds"),
-            Word("Sommarmorgon","zomerochtend","zweeds"),
-            Word("Gift","getrouwd","zweeds"),
-            Word("Planka","zwartrijden","zweeds")
-        )
-    }
+        Word("bil","auto","zweeds"),
+        Word("Vän","vriend","zweeds"),
+        Word("Bra","goed","zweeds"),
+        Word("Tystnad","stilte","zweeds"),
+        Word("Duktig","getalenteerd","zweeds"),
+        Word("Älskar","liefde","zweeds"),
+        Word("Leende","glimlach","zweeds"),
+        Word("Sommarmorgon","zomerochtend","zweeds"),
+        Word("Gift","getrouwd","zweeds"),
+        Word("Planka","zwartrijden","zweeds")
+    )
+
     fun play(){
-        println("You got it!")
-        println()
+        words.shuffle()
+        val five = words.take(5)
+        five.forEach{
+            println(it.original)
+        }
     }
 
 }
